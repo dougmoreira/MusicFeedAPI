684ce79510980443e3d1808cb1b6b4bd7649a386
Douglas Moreira <33124673+dougmoreira@users.noreply.github.com>
2023-04-15 09:34:27 -0300

    Creating new Project

---
+++ MusicFeedAPI/MusicFeedAPI/MusicFeedAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicFeedAPIApp.swift
+//  MusicFeedAPI
+//
+//  Created by Douglas Moreira on 15/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicFeedAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
